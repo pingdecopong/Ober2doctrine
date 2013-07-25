ac60da682f7888d8a1e7ed3ec523d4697c9b5877
pingdecopong <pingdecopong@gmail.com>
2013-07-25 09:32:32 +0900

    バンドルフォルダ移動

--- src/Arte/Ober2doctrineBundle/ArteOber2doctrineBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Arte\Ober2doctrineBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ArteOber2doctrineBundle extends Bundle
-{
-}
--- src/Arte/Ober2doctrineBundle/README.md
+++
@@ -1,2 +0,0 @@
-
-Ober2doctrineBundle
